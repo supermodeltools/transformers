d36434587bf3dcc2561c4bced3aff4c677fca065
Matt <rocketknight1@gmail.com>
2026-01-21 16:21:38 +0000

    Restore fillmaskpipeline tests

--- tests/models/reformer/test_modeling_reformer.py
+++ tests/models/reformer/test_modeling_reformer.py
@@ -735,6 +735,7 @@ class ReformerLSHAttnModelTest(
     pipeline_model_mapping = (
         {
             "text-embedding": ReformerModel,
+            "fill-mask": ReformerForMaskedLM,
             "text-classification": ReformerForSequenceClassification,
             "text-generation": ReformerModelWithLMHead,
             "zero-shot": ReformerForSequenceClassification,
